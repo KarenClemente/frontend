771ca323d7e93384e8be21e6cebc74b840873439
varandaeng <varandaeng@gmail.com>
2018-11-07 15:11:48 -0200

    post

--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -20,9 +20,61 @@ export class PostComponent {
   ambienteIn: boolean = false;
   ambienteOut: boolean = false;
   private selectedLink: string=" ";
+  public postsSimilar: Array<any>;
+  public likedPosts: Array<any>;
 
   
-    constructor(private _router: Router) {}
+    constructor(private _router: Router) {
+      this.postsSimilar = [];
+      this.likedPosts = [];  
+
+      this.postsSimilar.push(
+        {
+         id:0,
+         title:'Poste perigoso', 
+         image:'./assets/img/poste.jpeg',
+         location:'ICC Norte',
+         description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+         likes:11,
+         commentnum:2,
+         ranking:1,
+         liked:true,
+         date:'11/11/2018',
+         hour:'16:11',
+         user:{
+           name:'Funaaa',
+           profilePicture:'./assets/img/avatar.png',
+         },
+         comment:{
+          user:{name:'Gabriela',
+          profilePicture:'./assets/img/avatar.png',
+        }, 
+          com:'hahahaha vish que coisa!',
+        }},
+        {
+          id:1,
+          title:'Poste perigoso', 
+          image:'./assets/img/poste.jpeg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+        )  
+    }
 
     proxcard(){
    
