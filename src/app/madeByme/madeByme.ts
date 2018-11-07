aadf2a63a1d5e9b3445daae2f284d34f6174d89a
varandaeng <varandaeng@gmail.com>
2018-11-06 23:53:34 -0200

    cards

--- src/app/madeByme/madeByme.ts
+++ src/app/madeByme/madeByme.ts
@@ -8,6 +8,60 @@ import { Router } from '@angular/router'; // Added
 })
 export class MadeByMeComponent{
 
-    constructor(private _router: Router) {}
+  public posts: Array<any>;
+  public likedPosts: Array<any>;
+
+    constructor(private _router: Router) {
+      
+      this.posts = [];
+      this.likedPosts = [];  
+
+      this.posts.push(
+        {
+         id:0,
+         title:'Poste perigoso', 
+         image:'./assets/img/poste.jpeg',
+         location:'ICC Norte',
+         description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+         likes:11,
+         commentnum:2,
+         ranking:1,
+         liked:true,
+         date:'11/11/2018',
+         hour:'16:11',
+         user:{
+           name:'Funaaa',
+           profilePicture:'./assets/img/avatar.png',
+         },
+         comment:{
+          user:{name:'Gabriela',
+          profilePicture:'./assets/img/avatar.png',
+        }, 
+          com:'hahahaha vish que coisa!',
+        }},
+        {
+          id:1,
+          title:'Poste perigoso', 
+          image:'./assets/img/poste.jpeg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+        )      
+  }
 
 }
--- src/app/ranking/ranking.ts
+++ src/app/ranking/ranking.ts
@@ -8,6 +8,232 @@ import { Router } from '@angular/router'; // Added
 })
 export class RankingComponent {
 
-    constructor(private _router: Router) {}
+  public posts: Array<any>;
+  public likedPosts: Array<any>;
+
+    constructor(private _router: Router) {
+
+      this.posts = [];
+      this.likedPosts = [];  
+
+      this.posts.push(
+        {
+         id:1,
+         title:'Janela Quebrada', 
+         image:'./assets/img/janela.jpg',
+         location:'ICC Norte',
+         description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+         likes:11,
+         commentnum:2,
+         ranking:1,
+         liked:true,
+         date:'11/11/2018',
+         hour:'16:11',
+         user:{
+           name:'Funaaa',
+           profilePicture:'./assets/img/avatar.png',
+         },
+         comment:{
+          user:{name:'Gabriela'}, 
+          com:'hahahaha vish que coisa!',
+        }},
+        {
+          id:2,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+         {
+          id:3,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},{
+          id:4,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+         {
+          id:5,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+         {
+          id:6,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+         {
+          id:7,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},{
+          id:8,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+         {
+          id:9,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+         {
+          id:10,
+          title:'Janela Quebrada', 
+          image:'./assets/img/janela.jpg',
+          location:'ICC Norte',
+          description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
+          likes:11,
+          commentnum:2,
+          ranking:1,
+          liked:true,
+          date:'11/11/2018',
+          hour:'16:11',
+          user:{
+            name:'Funaaa',
+            profilePicture:'./assets/img/avatar.png',
+          },
+          comment:{
+           user:{name:'Gabriela',
+           profilePicture:'./assets/img/avatar.png',
+          }, 
+           com:'hahahaha vish que coisa!',
+         }},
+        )
+    }
    
 }
