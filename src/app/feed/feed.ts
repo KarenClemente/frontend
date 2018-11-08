ca95d07e2c3c27e9a16d5f4d5770bb6e4640be9b
karen <karen.clemente@gmail.com>
2018-11-08 16:30:10 -0200

    infinite scroll

--- src/app/feed/feed.ts
+++ src/app/feed/feed.ts
@@ -3,26 +3,31 @@ import { Router } from '@angular/router'; // Added
 import { ServerProvider} from '../../providers/server';
 import { Observable } from 'rxjs';
 
+
+
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.html',
   styleUrls: ['./feed.css']
 })
 
+
+
 export class FeedComponent {
+
+  onScroll () {
+      console.log('scrolled!!')
+      this.getPosts();
+  }
 
   public posts: Array<any>;
   public likedPosts: Array<any>;
 
-    constructor(private _router: Router) {
-      
-      this.posts = [];
-      this.likedPosts = [];  
-
+  getPosts(){
       this.posts.push(
         {
          id:0,
-         title:'Poste perigoso', 
+         title:'Poste perigoso',
          image:'./assets/img/poste.jpeg',
          location:'./assets/img/poste.jpeg',
          description:'./assets/img/poste.jpeg',
@@ -37,12 +42,41 @@ export class FeedComponent {
            profilePicture:'./assets/img/avatar.png',
          },
          comment:{
-          user:{name:'Gabriela'}, 
+          user:{name:'Gabriela'},
+          com:'hahahaha vish que coisa!',
+        }},
+      );
+  }
+
+    constructor(private _router: Router) {
+
+      this.posts = [];
+      this.likedPosts = [];
+
+      this.posts.push(
+        {
+         id:0,
+         title:'Poste perigoso',
+         image:'./assets/img/poste.jpeg',
+         location:'./assets/img/poste.jpeg',
+         description:'./assets/img/poste.jpeg',
+         likes:11,
+         commentnum:2,
+         ranking:1,
+         liked:true,
+         date:'11/11/2018',
+         hour:'16:11',
+         user:{
+           name:'Funaaa',
+           profilePicture:'./assets/img/avatar.png',
+         },
+         comment:{
+          user:{name:'Gabriela'},
           com:'hahahaha vish que coisa!',
         }},
         {
           id:1,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -59,11 +93,11 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},{
           id:2,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -80,11 +114,11 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},{
           id:3,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -101,11 +135,11 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},{
           id:4,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -122,12 +156,12 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},
          {
           id:5,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -144,12 +178,12 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},
          {
           id:6,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -166,12 +200,12 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},
          {
           id:7,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -188,11 +222,11 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},{
           id:8,
-          title:'Poste perigoso', 
+          title:'Poste perigoso',
           image:'./assets/img/poste.jpeg',
           location:'ICC Norte',
           description:'dsnj si dk dshbh sh h js dsbshubsuhbsdhisijsn ihsbhbsh sih ihs iha sihabihs ish s su sa hdsha sia d si',
@@ -209,11 +243,13 @@ export class FeedComponent {
           comment:{
            user:{name:'Gabriela',
            profilePicture:'./assets/img/avatar.png',
-          }, 
+          },
            com:'hahahaha vish que coisa!',
          }},
-        )      
+        )
+
+
   }
 
+
 }
-    
