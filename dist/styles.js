6aa7ddab44a1195bc2573c29af931492705fddeb
varandaeng <varandaeng@gmail.com>
2018-11-12 10:07:39 -0200

    sidebar

--- dist/styles.js
+++ dist/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Karen Clemente\desktop\sitesos\frontend\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\Mariana\Desktop\angular\frontend\src\styles.css */"./src/styles.css");
 
 
 /***/ })
